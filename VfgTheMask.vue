7392bb0f44bc3d51dff73d356b17d4d545ae4a0a
Gustavo Gondim <gustavospgondim@gmail.com>
2019-07-23 19:03:16 -0300

    First commit

---
+++ VfgTheMask.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <the-mask 
+      class="form-control"
+      :mask="schema.mask"
+      v-model="value"
+      :placeholder="schema.placeholder"
+      :tokens="schema.tokens"
+      ></the-mask>
+  </div>
+</template>
+<script>
+import { abstractField } from 'vue-form-generator';
+
+export default {
+  mixins: [ abstractField ]
+}
+</script>
